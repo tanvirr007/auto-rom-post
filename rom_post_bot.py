7da374157bc189fdcebb0f5618aead6e9f494d0a
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-12-12 18:33:05 +0600

    py: Add caption formatting function
    Change-Id: I810377f062c56c04a0720eeaa8162e72d05c0109
    Signed-off-by: tanvirr007 <tanvir.mymnsngh@gmail.com>

--- rom_post_bot.py
+++ rom_post_bot.py
@@ -25,3 +25,40 @@ def validate_bot_token(bot_token):
     if response.status_code != 200:
         raise ValueError("Seems like your Telegram bot token is wrong.")
     return response.json()
+
+def format_caption():
+    """Generate the formatted text template."""
+    today = datetime.now().strftime("%d/%m/%y")
+    caption = f"""
+
+#crDroid #spes #spesn #A15 #VIC #UNOFFICIAL #OSS
+*crDroid 11.x | UNOFFICIAL | Android 15*
+*Released:* _{today}_'
+
+▪️Download
+▪️Screenshots
+▪️[Support Group](https://t.me/TanvirBuildsSupport)
+▪️[Update Channel](https://t.me/Tanvir_CI)
+
+*Changelog:*
+• Type Here
+• Type Here
+
+*Notes:*
+• Don't forget to backup your important data
+• Use [PIF](https://t.me/TanvirBuilds/435) if needed
+• Use official [OrangeFox](https://orangefox.download/device/spes) recovery
+• Report bugs with proper logs
+• If you like our work, consider [donating](https://github.com/Team-Remix/.github/blob/main/donation%2FDONATION.md) to support server costs
+• This is an automatically generated post
+
+*Credits:*
+• All spes devs for resources
+• All of my testers for testing
+• Special thanks to God, Sun, Time, Love and Spes
+
+*By* [@tanvirr007](https://t.me/tanvirr007)
+*Follow* [@TanvirBuilds](https://t.me/TanvirBuilds)
+*Join* [@TanvirBuildsSupport](https://t.me/TanvirBuildsSupport)
+"""
+    return caption
